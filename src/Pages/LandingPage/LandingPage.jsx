0796a81991d421be006a6c79b325cd2e2e8207f9
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-08 05:55:46 -0500

    imported the css file and about to proceed with this page

---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './LandingPage.css'
+const Landingpage = () => {
+  return (
+    <div>
+        <h1>Landing Page</h1>
+    </div>
+  )
+}
+
+export default Landingpage
