fb31ccabc52cad05c0697ba9d6d5073d6b4b01e8
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-08 07:41:34 -0500

    Added a background image to the hero section

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import './LandingPage.css';
 import NavBar from '../../Components/NavBar/navBar'
 
+import searchicon from '../../Assets/search-icon.svg'
+
 
 const Landingpage = () => {
   return (
     <div >
+
+      {/* Hero section of the landing page */}
       <section className='hero'>
-          <NavBar />
+    
+        {/* Navbar Component */}
+        <div className='navcomponent'>
+            <NavBar />
+        </div>
+
+        <h1 className='heroheading'>Renting done right finally</h1>
+        
+        <div className='inputdiv'>
+          <input type="search" name="search" id="herosearch" placeholder='Where do you want to live?.' />
+          <img src={searchicon} alt="searchicon" className='searchicon'/>
+
+        </div>
+
+        <div>
+
+        </div>
 
       </section>
-        <h1>Landing Page</h1>
+
     </div>
   )
 }
