f7cefd2d675186386fcb5afde66bf9824604242e
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-11 15:41:02 -0500

    Imported useState to be used for the burger toggle

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import './navBar.css'
-import Logo from '../../Assets/EasyRent.svg'
-import { Link } from 'react-router-dom'
+import './navBar.css';
+import Logo from '../../Assets/EasyRent.svg';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const navBar = () => {
+
+  // Usestate for Hamburger display
+  const [toggle, setToggle] = useState(false)
+
+  const handleToggle = () => {
+    return setToggle(prevToggle =>!prevToggle)
+  }
+
   return (
    <div className='navBar'>
       <div className="navlogo">
@@ -21,7 +30,7 @@ const navBar = () => {
 
       <div >
         <Link to='./'><button className="navbutton" id='login'>Log In</button></Link>
-        <Link to='./'><button className="navbutton">Sign Up</button></Link>
+        <Link to='./'><button className="navbutton" id='signup'>Sign Up</button></Link>
       </div>
 
     </div>
