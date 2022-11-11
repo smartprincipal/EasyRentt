3d36d76977ad5e72abee5bc3e005a71e2098ee3a
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-11-10 21:34:27 -0800

    iImplemented the hero ectionofthe Catalogue Page

--- src/Pages/Catalogue/Catalogue.jsx
+++ src/Pages/Catalogue/Catalogue.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
-import'./Catalogue.css'
+import './Catalogue.css'
+// import NavBar from '../../Components/NavBar/navBar'
+import SearchImg from '../../Assets/searchImg.png'
 
 const Catalogue = () => {
   return (
     <div className='calaogue'>
      <div className="heroSection">
+      <div className='navBarr'>
+        {/* <NavBar/> */}
+      </div>
+      <div className="searchSection">
+        <select name="Price" id="Price" value='Price' className='ops'>
+        <option value="Price">Price</option>
+        </select>
+        <select name="Bed" id="Bed" value='Bed' className='ops'>
+        <option value="Bed">Bed</option>
+        </select>
+        <select name="Bathroom" id="Bathroom" value='Bathroom' className='ops'>
+        <option value="Bathroom">Bathroom</option>
+        </select>
+        
+        <div className="catalogueSearch">
+          <input 
+          type="search" 
+          name="Search" 
+          id="" 
+          className='searchBox' 
+          placeholder='Where do you want to live'
+          />
+          
+          <div className="searchimg">
+            <img src={SearchImg} alt="" />
+          </div>
+        </div>
+        <div className="sortBy">
+          <label htmlFor="">Sort by:</label>
+          <select name="Best Match" id="Best Match" value='Best Match' className='opsort'>
+        <option value="Best Match">Best Match</option>
+        </select>
+        </div>
+      </div>
       
      </div>
+
 
     </div>
   )
