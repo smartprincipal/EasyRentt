8c360307baeb3c35db52108832d20aaafb6c2436
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-13 14:24:24 -0500

    Made neccessary adjustments to this component

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -15,24 +15,33 @@ const Navbar = () => {
 
   
   return (
-   <div className='navBar'>
+   <div className='navbar'>
       <div className="navlogo">
         <img src={Logo} alt="EasyRent-Logo" />
       </div>
 
       <div className={toggle ? 'active' : 'menuList' }>
         <ul >
-          <li><Link to='./'>Rent</Link></li>
-          <li><Link to='./'>Buy</Link></li>
-          <li><Link to='./'>Blog</Link></li>
-          <li><Link to='./'>Partner With Us</Link></li>
+          <div className='navdiv'>
+            <div className='navlinks'>
+              <li><Link to='./'>Rent</Link></li>
+              <li><Link to='./'>Buy</Link></li>
+            </div>
+            <div className='navlinks'>
+              <li><Link to='./'>Blog</Link></li>
+              <li><Link to='./' id='special'>Partner With Us</Link></li>
+            </div>
+          </div>
+
+          <div className='buttondiv'>
+            <button className="navbutton" id='login'><Link to='./'>Log In</Link></button>
+            <button className="navbutton" id='signup'><Link to='./'>Sign Up</Link></button>
+          </div>
         </ul>
+      
+        
       </div>
 
-      <div >
-        <Link to='./'><button className="navbutton" id='login'>Log In</button></Link>
-        <Link to='./'><button className="navbutton" id='signup'>Sign Up</button></Link>
-      </div>
 
       <div id="hamburger" onClick={handleToggle}>
               {toggle ? <div>&times;</div> :  <div>&#9776;</div>}
