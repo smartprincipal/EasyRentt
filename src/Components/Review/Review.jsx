225aa6640d418e6e5babc0e7f8ee12e5e2d30ea0
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-20 08:56:16 -0500

    Destructured Reviewcard to be passed as Props

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -5,18 +5,18 @@ import review2 from '../../Assets/review2.svg';
 import review3 from '../../Assets/review3.svg';
 import review4 from '../../Assets/review4.svg';
 
-const Review = () => {
+const Review = ({img, heading, content}) => {
   return (
-    <div className='review'>
-        
-        <p className='reviewheading'>Review</p>
+    <div className='reviewcard'>
 
-        <div className='reviewcard'>
-          <img src={review1} alt="reviewcard" />
-          <img src={review2} alt="reviewcard" />
+            <img src={img} alt="reviewcard" />
+            <h1 className='reviewheading'>{heading}</h1>
+            <p className='reviewcontent'>{content}</p>
+          
+          {/* <img src={review2} alt="reviewcard" />
           <img src={review3} alt="reviewcard" />
-          <img src={review4} alt="reviewcard" />
-        </div>
+          <img src={review4} alt="reviewcard" /> */}
+       
     
     </div>
   )
