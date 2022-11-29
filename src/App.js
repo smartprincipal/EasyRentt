c9d87e4a82503d29ef303a93455f02744e808eea
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-29 06:53:17 -0500

    Added new dependency for select filter

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import Catalogue from "../src/Pages/Catalogue/Catalogue";
 import SignUp from "./Pages/Signup/Signup";
 import { Routes, Route } from "react-router-dom";
 import Overview from "./Components/Overview/Overview";
+import CatalogueNavbar from "./Components/CatalogueNavbar/CatalogueNavbar";
+
 
 
 function App() {
   return (
     <div>
-      <Routes>
-        {/* <Route path="/" element={<Landingpage />} /> */}
+      {/* <Routes>
+        <Route path="/" element={<Landingpage />} />
         <Route path="/" element={<Catalogue />} />
         <Route path="/Signup" element={<SignUp />} />
-      </Routes>
-      {/* <Overview /> */}
-      {/* <Login /> */}
-      {/* <Landingpage /> */}
-      {/* <Catalogue/>  */}
-      {/* <SignUp /> */}
+      </Routes> */}
+      <CatalogueNavbar />
 
     </div>
   );
---
+++ src/Components/CatalogueNavbar/CatalogueNavbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './CatalogueNavbar.css';
+import NavBar from '../NavBar/NavBar';
+import { Select, FormControl, MenuItem, InputLabel } from '@mui/material';
+
+const CatalogueNavbar = () => {
+  return (
+    <div className='catnavbar'>
+        <div className='navcontainer'>
+            <NavBar />
+        </div>
+        
+        <div>
+            <div>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Price</InputLabel>
+                    <Select id="demo-simple-select"  label="Price">
+                        <MenuItem value={1}>#250,000 - 500,000</MenuItem>
+                        <MenuItem value={2}>#500,000 - 1,000,000</MenuItem>
+                        <MenuItem value={3}>#1,000,000 - 5,000,000</MenuItem>
+                        <MenuItem value={4}>#5,000,000 - 10,000,000</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Beds</InputLabel>
+                    <Select id="demo-simple-select"  label="Beds">
+                        <MenuItem value={1}>Any</MenuItem>
+                        <MenuItem value={2}>Studio</MenuItem>
+                        <MenuItem value={3}>1</MenuItem>
+                        <MenuItem value={4}>2</MenuItem>
+                        <MenuItem value={5}>3</MenuItem>
+                        <MenuItem value={6}>4+</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Baths</InputLabel>
+                    <Select id="demo-simple-select"  label="Baths">
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3+</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CatalogueNavbar
