bde68a758827b82484a31d5db1a8edb722816ea2
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-12-04 16:56:12 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { Routes, Route } from "react-router-dom";
 import Landingpage from "./Pages/LandingPage/LandingPage";
 import Login from "./Pages/Login/Login";
 import Catalogue from "../src/Pages/Catalogue/Catalogue";
-import SignUp from "./Pages/Signup/Signup";
-import CatalogueNavbar from "./Components/CatalogueNavbar/CatalogueNavbar";
-import CatalogueCard from "./Components/CatalogueCard/CatalogueCard";
+// import SignUp from "./Pages/Signup/Signup";
+// import CatalogueNavbar from "./Components/CatalogueNavbar/CatalogueNavbar";
+// import CatalogueCard from "./Components/CatalogueCard/CatalogueCard";
 import Maryland from "./Pages/Catalogue/Maryland";
 
 
