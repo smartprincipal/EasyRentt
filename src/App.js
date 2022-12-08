737ae15f457b278c046c209b9dc42c784573d6ef
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-12-08 09:44:36 -0800

    implemented routing to APP.JS

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 // import Schedule from "./Pages/SchedulePage/Schedule";
 import Landingpage from "./Pages/LandingPage/LandingPage";
-// import Login from "./Pages/Login/Login";
 import Catalogue from "../src/Pages/Catalogue/Catalogue";
 import SignUp from "./Pages/Signup/Signup";
-import Landingpage from "./Pages/LandingPage/LandingPage";
 import Login from "./Pages/Login/Login";
-import Catalogue from "../src/Pages/Catalogue/Catalogue";
-import SignUp from "./Pages/Signup/Signup";
 // import CatalogueNavbar from "./Components/CatalogueNavbar/CatalogueNavbar";
 import CatalogueCard from "./Components/CatalogueCard/CatalogueCard";
 import Ajah from "./Pages/Catalogue/Ajah";
@@ -23,29 +19,8 @@ import Yaba from "./Pages/Catalogue/Yaba";
 import Viewmore from "./Pages/Viewmore/Viewmore"
 import Map from "./Components/Map/Map";
 import ViewMap from "./Pages/ViewMap/ViewMap";
-// import SchedulePage from "./Pages/SchedulePage/Schedule";
+import SchedulePage from "./Pages/SchedulePage/Schedule";
 import PaymentPersonalInfo from "./Pages/PaymentPersonalInfo/PaymentPersonalInfo";
-// import ViewMap from "./Pages/ViewMap/ViewMap";
-import Ikeja from './Pages/Catalogue/Ikeja'
-import Lekki from './Pages/Catalogue/Lekki'
-import Magodo from './Pages/Catalogue/Magodo'
-import Oshodi from './Pages/Catalogue/Oshodi'
-import Ajah from './Pages/Catalogue/Ajah'
-import Bananaisland from './Pages/Catalogue/Bananaisland'
-import Yaba from './Pages/Catalogue/Yaba'
-import Maryland from './Pages/Catalogue/Maryland'
-import Victoriaisland from './Pages/Catalogue/Victoriaisland'
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -66,18 +41,17 @@ function App() {
         <Route path="/Victoriaisland" element={<Victoriaisland />} />
         <Route path="/Yaba" element={<Yaba />} />
         <Route path="/PaymentPersonalInfo" element={<PaymentPersonalInfo />} />
+        <Route path="/SchedulePage" element={<SchedulePage />} />
+        <Route path="/ViewMap" element={<ViewMap />} />
+        <Route path="/Viewmore" element={<Viewmore />} />
+        <Route path="/Map" element={<Map />} />
+
+
+
+
+
 
       </Routes>
-      {/* <Overview /> */}
-      {/* <Login /> */}
-      <Landingpage />
-      {/* <Catalogue/> */}
-      {/* <SignUp /> */}
-      {/* <Viewmore/> */}
-      {/* <SchedulePage/> */}
-      <PaymentPersonalInfo/>
-      {/* <Map/> */}
-      {/* <ViewMap/> */}
     </div>
   );
 }
